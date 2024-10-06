cb230ae5d8addcbfda2737c875861f74318e872c
sada <g-g2g-g78@mail.ru>
2024-10-06 22:30:55 +0300

    upd3

--- models/user.go
+++ models/user.go
@@ -1,8 +1,11 @@
 package models
 
+import "gorm.io/gorm"
+
 // User представляет данные пользователя.
 // swagger:model
 type User struct {
+	gorm.Model
 	// Имя пользователя
 	// example: John Doe
 	Name string `json:"name"`
